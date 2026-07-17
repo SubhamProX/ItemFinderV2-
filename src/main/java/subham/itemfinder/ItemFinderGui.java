c5067438e10ea13d87db1d4a5e41f7b55baf0377
SubhamProX <subhamgamingmaharana@gmail.com>
2026-07-17 12:05:00 +0530

    Add files via upload

---
+++ src/main/java/subham/itemfinder/ItemFinderGui.java
@@ -0,0 +1,22 @@
+package subham.itemfinder;
+
+import io.github.cottonmc.cotton.gui.widget.WGridPanel;
+import io.github.cottonmc.cotton.gui.widget.WLabel;
+import io.github.cottonmc.cotton.gui.widget.WTextField;
+import io.github.cottonmc.cotton.gui.client.LightweightGuiDescription;
+import net.minecraft.network.chat.Component;
+
+public class ItemFinderGui extends LightweightGuiDescription {
+	public ItemFinderGui(){
+		WGridPanel root = new WGridPanel();
+		setRootPanel(root);
+		root.setSize(150, 130);
+		
+		WLabel helloLabel = new WLabel(Component.literal("Hello"));
+        root.add(helloLabel, 0, 0, 4, 1);
+		
+		WTextField searchBox = new WTextField(Component.literal("Search..."));
+        root.add(searchBox, 0, 1, 4, 1);
+        root.validate(this);
+    }
+}
