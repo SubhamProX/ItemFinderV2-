c51d0f52b63092f6612d18f7e7292f34b6776e5f
SubhamProX <subhamgamingmaharana@gmail.com>
2026-07-17 10:52:30 +0530

    Update ItemFinderClient.java

--- src/main/java/subham/itemfinder/ItemFinderClient.java
+++ src/main/java/subham/itemfinder/ItemFinderClient.java
@@ -2,11 +2,10 @@ package subham.itemfinder;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.KeyMapping;
 import com.mojang.blaze3d.platform.InputConstants;
-import org.lwjgl.glfw.GLFW;
 
 public class ItemFinderClient implements ClientModInitializer {
 
@@ -14,11 +13,11 @@ public class ItemFinderClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        openFinderKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
+        openFinderKey = KeyMappingHelper.registerKeyMapping(new KeyMapping(
             "key.itemfinder.open",
-            InputConstants.Type.KEYSYM,
-            GLFW.GLFW_KEY_Y,
-            "key.categories.misc"
+            InputConstants.Type.KEYBOARD,
+            InputConstants.KEY_Y,
+            KeyMapping.Category.MISC
         ));
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
