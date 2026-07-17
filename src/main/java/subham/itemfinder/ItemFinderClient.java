f5aa944ff8df9aa43fa45649eb1c1217ca9e0bfe
SubhamProX <subhamgamingmaharana@gmail.com>
2026-07-17 10:31:23 +0530

    Update ItemFinderClient.java

--- src/main/java/subham/itemfinder/ItemFinderClient.java
+++ src/main/java/subham/itemfinder/ItemFinderClient.java
@@ -1,24 +1,19 @@
 package subham.itemfinder;
 
-import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.KeyMapping;
+import com.mojang.blaze3d.platform.InputConstants;
 import org.lwjgl.glfw.GLFW;
 
 public class ItemFinderClient implements ClientModInitializer {
-	private static KeyBinding openFinderKey;
-
-	@Override
-	public void onInitializeClient() {
-		openFinderKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-			"key.itemfinder.open",
-			InputUtil.Type.KEYSYM,
-			GLFW.GLFW_KEY_O,
-			"category.itemfinder"
-		));
+	private static KeyMapping openFinderKey;
+openFinderKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
+    "key.itemfinder.open",
+    InputConstants.Type.KEYSYM,
+    GLFW.GLFW_KEY_Y,
+    "key.categories.misc"
+));
 
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			while (openFinderKey.wasPressed()) {
