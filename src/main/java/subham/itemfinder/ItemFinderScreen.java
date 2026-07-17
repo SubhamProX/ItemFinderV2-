dd302f6ced897dd0dab58175e37b4e92d9110dbe
SubhamProX <subhamgamingmaharana@gmail.com>
2026-07-17 10:53:22 +0530

    Update ItemFinderScreen.java

--- src/main/java/subham/itemfinder/ItemFinderScreen.java
+++ src/main/java/subham/itemfinder/ItemFinderScreen.java
@@ -3,7 +3,8 @@ package subham.itemfinder;
 import io.github.cottonmc.cotton.gui.client.CottonClientScreen;
 
 public class ItemFinderScreen extends CottonClientScreen {
-	public ItemFinderScreen() {
-		super(new ItemFinderGui());
-	}
+
+    public ItemFinderScreen() {
+        super(new ItemFinderGui());
+    }
 }
