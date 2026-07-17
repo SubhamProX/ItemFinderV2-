50fca92eae5571a621b78cdc308581c95f242e54
SubhamProX <subhamgamingmaharana@gmail.com>
2026-07-17 12:12:44 +0530

    Update ItemFinderScreen.java

--- src/main/java/subham/itemfinder/ItemFinderScreen.java
+++ src/main/java/subham/itemfinder/ItemFinderScreen.java
@@ -1,43 +1,23 @@
 package subham.itemfinder;
 
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
 
 public class ItemFinderScreen extends Screen {
-
-    private EditBox searchBox;
 
     public ItemFinderScreen() {
         super(Component.literal("Item Finder"));
     }
 
     @Override
-    protected void init() {
-        this.searchBox = new EditBox(
-            this.font,
-            this.width / 2 - 100,
-            this.height / 4,
-            200, 20,
-            Component.literal("Search items...")
-        );
-        this.searchBox.setHint(Component.literal("Search items..."));
-        this.addRenderableWidget(this.searchBox);
-    }
-
-    @Override
-    public void render(GuiGraphics graphics, int mouseX, int mouseY, float delta) {
-        // Modern versions only require the graphics context to render the background blur/dirt
-        this.renderBackground(graphics); 
-        
-        graphics.drawCenteredString(this.font, this.title, this.width / 2, 20, 0xFFFFFF);
-        super.render(graphics, mouseX, mouseY, delta);
+    public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float delta) {
+        super.render(guiGraphics, mouseX, mouseY, delta);
+        guiGraphics.drawCenteredString(this.font, "Hello", this.width / 2, this.height / 2, 0xFFFFFF);
     }
 
     @Override
     public boolean isPauseScreen() {
         return false;
     }
-            }
-        
+}
