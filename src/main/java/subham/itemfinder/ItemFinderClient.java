907326512337ad88e164873f2460dd7a86f201bf
SubhamProX <subhamgamingmaharana@gmail.com>
2026-07-17 13:25:18 +0530

    Update ItemFinderClient.java

--- src/main/java/subham/itemfinder/ItemFinderClient.java
+++ src/main/java/subham/itemfinder/ItemFinderClient.java
@@ -1,29 +1,41 @@
 package subham.itemfinder;
 
+import com.mojang.blaze3d.platform.InputConstants;
+import org.lwjgl.glfw.GLFW;
+
+import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
+import net.minecraft.resources.Identifier;
+
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.KeyMapping;
-import com.mojang.blaze3d.platform.InputConstants;
 
 public class ItemFinderClient implements ClientModInitializer {
+
+    private static final KeyMapping.Category CATEGORY = KeyMapping.Category.register(
+            Identifier.fromNamespaceAndPath("itemfinder", "custom_category")
+    );
 
     private static KeyMapping openFinderKey;
 
     @Override
     public void onInitializeClient() {
         openFinderKey = KeyMappingHelper.registerKeyMapping(new KeyMapping(
-            "key.itemfinder.open",
-            InputConstants.Type.KEYSYM,
-            InputConstants.KEY_Y,
-            KeyMapping.Category.MISC
+                "key.itemfinder.open",
+                InputConstants.Type.KEYSYM,
+                GLFW.GLFW_KEY_O,
+                CATEGORY
         ));
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (openFinderKey.consumeClick()) {
-                Minecraft.getInstance().gui.setScreen(new ItemFinderScreen());
+                Minecraft.getInstance().setScreen(new ItemFinderScreen());
             }
         });
+
+        // Chest highlight aur slot highlight systems register karo
+        ChestHighlightRenderer.register();
+        ChestSlotHighlighter.register();
     }
-}
+    }
