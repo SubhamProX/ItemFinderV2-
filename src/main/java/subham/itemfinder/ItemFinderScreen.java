de00a7df61c4df31889c6d50b16de66ba0879e39
SubhamProX <subhamgamingmaharana@gmail.com>
2026-07-17 12:20:34 +0530

    Update ItemFinderScreen.java

--- src/main/java/subham/itemfinder/ItemFinderScreen.java
+++ src/main/java/subham/itemfinder/ItemFinderScreen.java
@@ -1,19 +1,12 @@
 package subham.itemfinder;
 
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.network.chat.Component;
 
 public class ItemFinderScreen extends Screen {
 
     public ItemFinderScreen() {
         super(Component.literal("Item Finder"));
-    }
-
-    @Override
-    public void render(GuiGraphicsExtractor guiGraphics, int mouseX, int mouseY, float delta) {
-        super.render(guiGraphics, mouseX, mouseY, delta);
-        guiGraphics.drawCenteredString(this.font, "Hello", this.width / 2, this.height / 2, 0xFFFFFF);
     }
 
     @Override
