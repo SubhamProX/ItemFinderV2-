98f3ea5c292348662e0e9c5d2afcaf4fdcbfe094
SubhamProX <subhamgamingmaharana@gmail.com>
2026-07-17 10:37:48 +0530

    Update ItemFinderClient.java

--- src/main/java/subham/itemfinder/ItemFinderClient.java
+++ src/main/java/subham/itemfinder/ItemFinderClient.java
@@ -1,5 +1,7 @@
 package subham.itemfinder;
 
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.KeyMapping;
@@ -7,18 +9,22 @@ import com.mojang.blaze3d.platform.InputConstants;
 import org.lwjgl.glfw.GLFW;
 
 public class ItemFinderClient implements ClientModInitializer {
-	private static KeyMapping openFinderKey;
-openFinderKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
-    "key.itemfinder.open",
-    InputConstants.Type.KEYSYM,
-    GLFW.GLFW_KEY_Y,
-    "key.categories.misc"
-));
 
-		ClientTickEvents.END_CLIENT_TICK.register(client -> {
-			while (openFinderKey.wasPressed()) {
-				client.setScreen(new ItemFinderScreen());
-			}
-		});
-	}
+    private static KeyMapping openFinderKey;
+
+    @Override
+    public void onInitializeClient() {
+        openFinderKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
+            "key.itemfinder.open",
+            InputConstants.Type.KEYSYM,
+            GLFW.GLFW_KEY_Y,
+            "key.categories.misc"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (openFinderKey.consumeClick()) {
+                client.setScreen(new ItemFinderScreen());
+            }
+        });
+    }
 }
